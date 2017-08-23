3347678508fd02f69fa17a7fcb650742c52f8c25
jacob ruleaux and larry taylor <larry@>
2017-08-23 09:13:27 -0700

    build dummy animal objects, add category links to navbar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,6 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { AboutComponent } from './about/about.component';
 import { AnimalsComponent } from './animals/animals.component';
 import { MedicalComponent } from './medical/medical.component';
 import { EducationComponent } from './education/education.component';
@@ -10,28 +9,28 @@ import { ContactComponent } from './contact/contact.component';
 
 const appRoutes: Routes = [
   {
-    path: 'about',
-    component: About
+    path: '',
+    component: AboutComponent
   },
   {
     path: 'contact',
-    component: Contact
+    component: ContactComponent
   },
   {
     path: 'charity',
-    component: Charity
+    component: CharityComponent
   },
   {
     path: 'education',
-    component: Education
+    component: EducationComponent
   },
   {
     path: 'animals',
-    component: Animals
+    component: AnimalsComponent
   },
   {
     path: 'medical',
-    component: Medical
+    component: MedicalComponent
   }
 ];
 
---
+++ src/app/cause.model.ts
@@ -0,0 +1,3 @@
+export class Cause {
+  constructor( public category: string, public title: string, public description: string, public funding: number, public swag: string){}
+}
