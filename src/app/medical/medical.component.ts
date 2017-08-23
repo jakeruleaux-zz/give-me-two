20f2572e1764da95b27c29aa4f9d4fce34b06ff1
jacob ruleaux and larry taylor <larry@>
2017-08-23 09:23:21 -0700

    build out componet files to display the things that we need to see

--- src/app/medical/medical.component.ts
+++ src/app/medical/medical.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cause } from '../cause.model';
 
 @Component({
   selector: 'app-medical',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./medical.component.css']
 })
 export class MedicalComponent implements OnInit {
+  medicals: Cause[] = [
+    new Cause("animal", "Little Sally's Little Alligator", "Little Sally's alligator Gerald needs a new tail or he'll die.", 48029, "hat"),
+    new Cause("animal", "My Neighbor's bird", "The guys next door have a bird that screams all night. I need money to hire a bird hitman.", 13902, "hat with a feather"),
+    new Cause("animal", "Cat in a tree", "There's a cat in a tree. You've seen it before. It's not that special.", 980228, "the cat")
+  ];
 
   constructor() { }
 
